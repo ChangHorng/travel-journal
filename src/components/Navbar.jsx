db5d39a7f426ff03a0b634e5bcf869214833feb6
clee0091 <clee0091@student.monash.edu>
2022-11-29 17:08:29 +0800

    Implemented Navbar to Travel Journal

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="/images/main-logo.png" className="nav-logo" />
+            <h3 className="nav-title">my travel journal.</h3>
+        </nav>
+    )
+}
